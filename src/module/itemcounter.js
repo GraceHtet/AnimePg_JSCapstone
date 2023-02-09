1ce12d71a29f13b82e2f744b207d98b5f640cdfc
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2023-02-10 00:28:16 +0530

    Add test files

--- src/module/itemcounter.js
+++ src/module/itemcounter.js
@@ -1,16 +1,13 @@
 /* eslint-disable import/no-cycle */
 import { anidata } from './showList';
 
-const counter = document.querySelector('.anime-title');
-
 const itemCounter = () => {
+  const counter = document.querySelector('.anime-title');
   if (anidata.length !== 0) {
     counter.innerHTML = `Anime TV shows <span class="shows-tracker">(${anidata.length})</span>`;
   } else {
     counter.innerHTML = 'No shows added';
   }
 };
-
-console.log(anidata);
 
 export default itemCounter;
